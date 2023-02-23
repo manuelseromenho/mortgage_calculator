57b505c157fbe0f11c21215e5ae4589354e78bbb
Manuel Seromenho <manuel.seromenho@eturnity.ch>
2023-02-23 23:39:03 +0000

    updating settings

--- mortgage_calculator/settings.py
+++ mortgage_calculator/settings.py
@@ -84,6 +84,8 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
+# STATIC_URL = env("DJANGO_STATIC_URL", default="/static/")
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
